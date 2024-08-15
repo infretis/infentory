51387248e23a3547b06d2ac7ddb5bdcae3bb3275
dz24 <daniel.t.zhang@ntnu.no>
2024-08-15 10:19:30 +0200

    add runnable water boiling

--- water_boiling/Orderp.py
+++ water_boiling/Orderp.py
@@ -1,7 +1,13 @@
-"""This file defines the order parameter used for boiling."""
+# -*- coding: utf-8 -*-
+# Copyright (c) 2015, PyRETIS Development Team.
+# Distributed under the LGPLv3 License. See LICENSE for more info.
+"""This file defines the order parameter used for the hydrate example."""
 import logging
+import os
+import subprocess
+import numpy as np
 import mdtraj as md
-from pyretis.orderparameter import OrderParameter
+from infretis.classes.orderparameter import OrderParameter
 logger = logging.getLogger(__name__)  # pylint: disable=C0103
 logger.addHandler(logging.NullHandler())
 
@@ -22,14 +28,14 @@ class H20Hole(OrderParameter):
 
         """
         super().__init__(description='H2O-hole')
-        trj=md.load('gromacs_input/conf.gro')  # <---- shortcut, make sure the file is there
-        self.idx_o = trj.top.select("symbol == O")
+        # trj=md.load('gromacs_input/conf.gro')  # <---- shortcut, make sure the file is there
+        # self.idx_o = trj.top.select("symbol == O")
 
 
     def calculate(self, system):
         """Calculate the order parameter.
 
-        Here, the order parameter is just the minimal value of
+        Here, the order parameter is just the minimal value of 
         local H2O density in the thin film (2D density).
 
         Parameters
@@ -46,7 +52,6 @@ class H20Hole(OrderParameter):
             The order parameter list.
         """
 
-        box = system.box.cell
-        pos = system.particles.pos[self.idx_o, :]
-        dens = len(pos)/box[0]**3
+        dens = (len(system.pos)/3)/system.box[0]**3
+
         return [-dens*(18.0153/((10**-21)*6.022*10**23))]
