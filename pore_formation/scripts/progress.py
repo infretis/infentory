fcaf829d9f8fa1fb806f287a08ddc9f90f86b01c
dz24 <rubidxx.dz@gmail.com>
2025-12-23 09:13:23 +0900

    update

--- pore_formation/scripts/progress.py
+++ pore_formation/scripts/progress.py
@@ -33,11 +33,12 @@ for c0 in c:
     axes[1].plot(x0, -y0, ls="--", zorder=3000, color="k")
 
 axes[0].contour(x, y, ufree, colors='black', alpha=0.25, zorder=2000, levels=20)
+axes[1].contour(x, y, commi, colors='black', alpha=0.25, zorder=2000, levels=20)
 fifty_mask = (0.40 < commi) & (commi < 0.60)
 fifty_mask &= np.isfinite(commi)
 avg_hist_fifty = commi.copy()
 avg_hist_fifty[~fifty_mask] = np.inf
-axes[0].contourf(x, y, avg_hist_fifty, colors='r', alpha=0.25, zorder=100, levels=20)
+axes[1].contourf(x, y, avg_hist_fifty, colors='r', alpha=0.25, zorder=100, levels=20)
 
 
 x, y, xs, ys = [], [], [], []
@@ -47,7 +48,7 @@ for i in range(0, 1000):
     if not os.path.exists(path):
         break
     data = np.loadtxt(path)
-    if data[-1, 1] > 0:
+    if data[-1, 1] > c[1]:
         reactive = True
         break
     x += list(data[:, 2]) + [None]
@@ -58,12 +59,11 @@ axes[0].plot(x, y, color="C0", zorder=10)
 axes[0].scatter(xs, ys, edgecolor="k", zorder=11, color="C0")
 axes[1].plot(x, y, color="C0", zorder=10)
 axes[1].scatter(xs, ys, edgecolor="k", zorder=11, color="C0")
-skip = 1652
 if reactive:
-    axes[0].plot(data[:skip, 2], data[:skip, 3]/10, color="C1", zorder=9)
-    axes[0].scatter(data[:skip, 2][[0, -1]], data[:skip, 3][[0, -1]]/10, color="C1", zorder=12, edgecolor="k")
-    axes[1].plot(data[:skip, 2], data[:skip, 3]/10, color="C1", zorder=9)
-    axes[1].scatter(data[:skip, 2][[0, -1]], data[:skip, 3][[0, -1]]/10, color="C1", zorder=12, edgecolor="k")
+    axes[0].plot(data[:, 2], data[:, 3]/10, color="C1", zorder=9)
+    axes[0].scatter(data[:, 2][[0, -1]], data[:, 3][[0, -1]]/10, color="C1", zorder=12, edgecolor="k")
+    axes[1].plot(data[:, 2], data[:, 3]/10, color="C1", zorder=9)
+    axes[1].scatter(data[:, 2][[0, -1]], data[:, 3][[0, -1]]/10, color="C1", zorder=12, edgecolor="k")
 
 for i in range(1, 1000):
     path = f"../infretis_{i}.toml"
